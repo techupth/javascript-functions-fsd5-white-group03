540232a7df480642a27a5107400e39cabb7276da
apollo5436 <kittisakapollo@gmail.com>
2023-11-29 10:18:18 +0700

    HH-byTeTae

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,10 @@
-// Start coding here
+function findMinNumber (a,b){
+    /*if(a > b)
+        return (b);
+    if(b > a)
+        return (a);*/
+    return (Math.min(a,b));
+}
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
