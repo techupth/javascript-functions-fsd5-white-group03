b917d05ae74317764b524d4bbdd7d058467e3964
pednaruemol <naruemol.kphet@gmail.com>
2023-11-29 13:29:41 +0700

    finished exercise1-6(Function JS)

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,25 @@
 // Start coding here
+// refactor>>>function add(a, b) {return a + b;}
+let add = function (a, b) {
+  return a + b;
+};
+console.log(add(10, 5));
+
+// refactor>>>function subtract(a, b) {return a - b;}
+const subtract = function (a, b) {
+  return a - b;
+};
+console.log(subtract(10, 5));
+
+// refactor>>>function multiply(a, b) {return a * b;}
+const multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(10, 5));
+
+// refactor>>>function divide(a, b) {return a / b;}
+const divide = function (a, b) {
+  return a / b;
+};
+let result = divide(10, 5);
+console.log(result);
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,17 @@
 // Start coding here
+// refactor>>>function add(a, b) {return a + b;}
+let add = (a, b) => a + b;
+console.log(add(10, 5));
+
+// refactor>>>function subtract(a, b) {return a - b;}
+const subtract = (a, b) => a - b;
+console.log(subtract(10, 5));
+
+// refactor>>>function multiply(a, b) {return a * b;}
+const multiply = (a, b) => a * b;
+console.log(multiply(10, 5));
+
+// refactor>>>function divide(a, b) {return a / b;}
+const divide = (a, b) => a / b;
+let result = divide(10, 5);
+console.log(result);
